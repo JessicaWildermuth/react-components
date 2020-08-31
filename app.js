6d3d92287be43a855855d6f2c263b63323e51ae5
JessicaWildermuth <jwildermuth536@gmail.com>
2020-08-31 10:52:33 -0700

    Populate list items with props

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
 // TODO
-var GroceryList = () => {
+var App = () => {
   return (
   <div>
     <h1>My Grocery List</h1>
-    <ul>
-      <Wine />
-      <BubbleBath />
-      <Cheese />
-    </ul>
+    <GroceryList groceryitems={['Wine', 'Bubble Bath', 'Cheese']} />
   </div>
+  )
+}
+var GroceryList = (props) => {
+  return (
+    <ul>
+      <li>{props.groceryitems[0]}</li>
+      <li>{props.groceryitems[1]}</li>
+      <li>{props.groceryitems[2]}</li>
+    </ul>
+
   );
 }
 
-var Wine = () => {
-  return (
-    <li>Wine</li>
-  );
-}
+// var Wine = () => {
+//   return (
+//     <li>Wine</li>
+//   );
+// }
 
-var BubbleBath = () => {
-  return (
-    <li>Bubble Bath</li>
-  );
-}
-var Cheese = () => {
-  return (
-  <li>Cheese</li>
-  );
-}
+// var BubbleBath = () => {
+//   return (
+//     <li>Bubble Bath</li>
+//   );
+// }
+// var Cheese = () => {
+//   return (
+//   <li>Cheese</li>
+//   );
+// }
 
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
 
